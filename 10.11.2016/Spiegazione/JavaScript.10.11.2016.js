225b9fd694c5f997468b991ac8ddbbae1cf13b64
armando pezzimenti <armando pezzimenti>
2016-11-10 17:53:41 +0100

    ArmRandoM

---
+++ 10.11.2016/Spiegazione/JavaScript.10.11.2016.js
@@ -0,0 +1,8 @@
+var pluto = prompt("Dal file inserisci valore");
+
+var f1File = function(p)
+        {
+          var pippo;
+          pippo = prompt(p + " : Inserisci un valore");
+          alert("il valore di pippo è " + pippo);
+        }
