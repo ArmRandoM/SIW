8f71b0418130eea545e29ba2633b5c2ea1ae82a5
armando pezzimenti <armando pezzimenti>
2016-12-27 16:59:09 +0100

    ArmRandoM

--- 10.11.2016/ESE07JavaScript/gestoreContatti.js
+++ 10.11.2016/ESE07JavaScript/gestoreContatti.js
@@ -15,6 +15,34 @@ function init()
 
   var resetFieldsButton = document.getElementById("resetFields");
   resetFieldsButton.addEventListener('click', resetFields);
+
+  var searchButton = document.getElementById("search");
+  searchButton.addEventListener('click', search);
+}
+
+function search()
+{
+  var nameToSearch = prompt( "Insert FirstName" );
+  var lastNameToSearch = prompt( "Insert LastName" );
+
+  var found = false;
+  var contactIndex = 0;
+
+  for( var i = 0; i < contacts.length; i++)
+  {
+    if( contacts[i].firstName == nameToSearch && contacts[i].lastName == lastNameToSearch )
+    {
+      found = true;
+      contactIndex = i;
+    }
+  }
+
+  if( found == true )
+    alert( "Contact Found!!!" + "\nFirst Name: " + contacts[contactIndex].firstName + "\nLast Name: "
+          + contacts[contactIndex].lastName + "\nPhone: " + contacts[contactIndex].phone + "\nE-mail: "
+          + contacts[contactIndex].email + "\n");
+  else
+    alert( "Contact Not Found!" );
 }
 
 function resetFields()
@@ -39,27 +67,49 @@ function addContact()
   var tdLastName = document.createElement("td");
   var tdPhone = document.createElement("td");
   var tdEmail = document.createElement("td");
-  var tdAction = document.createElement("td");
+  var tdAction1 = document.createElement("td");
+  var tdAction2 = document.createElement("td");
   var deleteButton = document.createElement("button");
+  var modifyButton = document.createElement("button");
 
   deleteButton.addEventListener('click', function()
   {
     tBody.removeChild(tr);
   });
 
-  tdAction.appendChild(deleteButton);
+  modifyButton.addEventListener('click', function()
+  {
+    var newFirstName = prompt("Insert new First Name");
+    var newLastName = prompt("Insert new Last Name");
+    var newPhone = prompt("Insert new Phone");
+    var newEmail = prompt("Insert new E-mail");
+
+    if( newFirstName != "" && newFirstName != null )
+      tdFirstName.textContent = newFirstName;
+    if( newLastName != "" && newLastName != null )
+      tdLastName.textContent = newLastName;
+    if( newPhone != "" && newPhone != null )
+      tdPhone.textContent = newPhone;
+    if( newEmail != "" && newEmail.includes("@") && newEmail != null )
+      tdEmail.textContent = newEmail;
+  });
+
+  tdAction1.appendChild(deleteButton);
+  tdAction2.appendChild(modifyButton);
 
   tdFirstName.textContent = aContact.firstName;
   tdLastName.textContent = aContact.lastName;
   tdPhone.textContent = aContact.phone;
   tdEmail.textContent = aContact.email;
   deleteButton.textContent = "DeleteContact";
+  modifyButton.textContent = "modifyContact";
 
   tr.appendChild(tdFirstName);
   tr.appendChild(tdLastName);
   tr.appendChild(tdPhone);
   tr.appendChild(tdEmail);
-  tr.appendChild(tdAction);
+  tr.appendChild(tdAction1);
+  tr.appendChild(tdAction2);
 
   if( tdFirstName.textContent != "" && tdLastName.textContent != "" && tdPhone.textContent != "" && tdEmail.textContent != "")
     tBody.insertBefore(tr, lastRow);
